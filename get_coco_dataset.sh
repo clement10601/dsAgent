44c302132234ca04c700fe6841e662c0aa2df116
clement <clement10601@gmail.com>
2018-01-02 11:50:48 +0800

    add runtime hint

--- get_coco_dataset.sh
+++ get_coco_dataset.sh
@@ -2,6 +2,9 @@
 pip install -r requirements.txt
 p=`which virtualenvwrapper.sh`
 echo "$p"
+echo "========================"
+echo "Setup python environment"
+echo "========================"
 grep -q -F 'export WORKON_HOME=~/.virtualenvs' ~/.bashrc || echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
 grep -q -F 'source $p' ~/.bashrc || echo 'source /usr/bin/virtualenvwrapper.sh' >> ~/.bashrc
 source ~/.bashrc
@@ -16,6 +19,10 @@ which pip
 pip install -r requirements.txt
 cd PythonAPI
 make && make install
+
+echo "====================================="
+echo "Download and extract images from COCO"
+echo "====================================="
 cd ../..
 mkdir images
 mkdir downloads
@@ -29,6 +36,9 @@ unzip -q train2017.zip
 unzip -q val2017.zip
 unzip -q test2017.zip
 # rm -rf *.zip
+echo "=================="
+echo "Changing file name"
+echo "=================="
 cd train2017
 for f in *.jpg; do mv "$f" "COCO_train2017_$f"; done
 cd ..
@@ -38,18 +48,27 @@ cd ..
 cd test2017
 for f in *.jpg; do mv "$f" "COCO_test2017_$f"; done
 cd ..
+echo "============================"
+echo "Moving data to images folder"
+echo "============================"
 mv train2017 ../images
 mv val2017 ../images
 mv test2017 ../images
 # back to root
 cd ..
 
+echo "======================================="
+echo "Exporting absolute path list for images"
+echo "======================================="
 mkdir exports
 find images/train2017/ -exec readlink -f {} \; >  exports/train2017.txt
 find images/val2017/ -exec readlink -f {} \; >  exports/val2017.txt
 find images/test2017/ -exec readlink -f {} \; >  exports/test2017.txt
 # Download COCO Metadata
 cd downloads
+echo "=============================="
+echo "Download and unzip annotations"
+echo "=============================="
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 wget -c http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
 unzip -q annotations_trainval2017.zip
@@ -57,4 +76,9 @@ unzip -q stuff_annotations_trainval2017.zip
 
 mv annotations/ ..
 cd ..
+
+echo "============================================================"
+echo "Run Python Script to generate image information with cocoAPI"
+echo "============================================================"
+
 python pycoco.py
