081d665e22c5d55737b2d86fb113588b7e7be766
clement <clement10601@gmail.com>
2018-01-02 11:24:44 +0800

    add download folder

--- get_coco_dataset.sh
+++ get_coco_dataset.sh
@@ -18,8 +18,8 @@ cd PythonAPI
 make && make install
 cd ../..
 mkdir images
-cd images
-
+mkdir downloads
+cd downloads
 # Download Images
 wget -c http://images.cocodataset.org/zips/train2017.zip
 wget -c http://images.cocodataset.org/zips/val2017.zip
@@ -38,15 +38,18 @@ cd ..
 cd test2017
 for f in *.jpg; do mv "$f" "COCO_test2017_$f"; done
 cd ..
+mv train2017 ../images
+mv val2017 ../images
+mv test2017 ../images
 # back to root
 cd ..
+
 mkdir exports
 find images/train2017/ -exec readlink -f {} \; >  exports/train2017.txt
 find images/val2017/ -exec readlink -f {} \; >  exports/val2017.txt
 find images/test2017/ -exec readlink -f {} \; >  exports/test2017.txt
-
 # Download COCO Metadata
-cd exports
+cd downloads
 wget -c http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 wget -c http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip
 unzip -q annotations_trainval2017.zip
